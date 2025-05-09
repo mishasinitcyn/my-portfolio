5973332b96ce6d4fff54df09ffa09a8575555f1a
Mikhail Sinitcyn <msa242@sfu.ca>
2025-05-09 16:29:43 -0700

    DEV [Portfolio] Remove about me

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -312,7 +312,7 @@ const Portfolio = () => {
     <div className="min-h-screen relative overflow-hidden">
       <div className="relative z-10 container mx-auto px-4 py-4 sm:p-8">
         <ProfileHeader profilePicture={profilePicture} />
-        <AboutMe />
+        {/* <AboutMe /> */}
         <Experience />
         <PortfolioSection />
       </div>
